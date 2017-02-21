366d9061c6e12aeddc79a0efa8b62ed084e88634
Alex Skwarczynski <askwarcz@tamu.edu>
2017-02-21 13:01:30 -0600

    Fixed export problem in h file

--- Database/Database.h
+++ Database/Database.h
@@ -18,16 +18,16 @@ namespace Database
 	public:
 
 		// Create an empty record with size number of entries
-		Record(int size);
+		DATABASE_API Record(int size);
 
-		// Return the size of the record
-		int GetSize();
+		// Return the size of the record	
+		DATABASE_API int GetSize();
 
 		// Get an attribute of a record
-		std::string Get(int index);
+		DATABASE_API std::string Get(int index);
 
 		// Set an attribute of a record
-		void Set(int index, std::string new_value);
+		DATABASE_API void Set(int index, std::string new_value);
 
 	};
 
@@ -35,47 +35,47 @@ namespace Database
 	{
 	public:
 		// Constructs an empty table
-		Table();
+		DATABASE_API Table();
 
 		// Constructs a table with all attributes in thsi list
-		Table(std::vector<std::string> attributes);
+		DATABASE_API Table(std::vector<std::string> attributes);
 
 		// Adds an attribute column to the end of the table
-		void AddAttribute(std::string attribute_name);
+		DATABASE_API void AddAttribute(std::string attribute_name);
 
 		// Deletes an attribute column from the table
-		void DeleteAttribute(std::string attribute_name);
+		DATABASE_API void DeleteAttribute(std::string attribute_name);
 
 		// Inserts a record into the table
-		void InsertRecord(Record new_record);
+		DATABASE_API void InsertRecord(Record new_record);
 
 		// Returns a list of all attributes in the table
-		std::vector<std::string> ListAttributes();
+		DATABASE_API std::vector<std::string> ListAttributes();
 
 		// Get the size (number of records from the table)
-		int GetSize();
+		DATABASE_API int GetSize();
 
 		// Get an pointer to the first record, returns NULL if table is empty
 		// This pointer can then be used to iterate through the table
-		Record* GetFirstRecord();
+		DATABASE_API Record* GetFirstRecord();
 
 		// Designate the key_name to be the key
-		void DesignateKey(std::string key_name);
+		DATABASE_API void DesignateKey(std::string key_name);
 
 		// cross join joining table with this table
-		Table CrossJoin(Table joining_table);
+		DATABASE_API Table CrossJoin(Table joining_table);
 
 		// natural join joining table with this table
-		Table NaturalJoin(Table joining_table);
+		DATABASE_API Table NaturalJoin(Table joining_table);
 
 		// Count the non-null entries of this attribute
-		int Count(std::string attribute_name);
+		DATABASE_API int Count(std::string attribute_name);
 
 		// Compute the maximum of the entries of this attribute
-		int Max(std::string attribute);
+		DATABASE_API int Max(std::string attribute);
 
 		// Compute the minimum of the entries of this attribure
-		int Min(std::string attribute);
+		DATABASE_API int Min(std::string attribute);
 
 	};
 
@@ -85,22 +85,22 @@ namespace Database
 	public:
 
 		// Construct an empty database
-		Database();
+		DATABASE_API Database();
 
 		// Add a table with name table_name to the database
-		void Add(std::string table_name, Table table);
+		DATABASE_API void Add(std::string table_name, Table table);
 
 		// Drop table table_name from the database
-		void Drop(std::string table_name);
+		DATABASE_API void Drop(std::string table_name);
 
 		// Returns a list of table names
-		std::vector<std::string> ListTables();
+		DATABASE_API std::vector<std::string> ListTables();
 
 		// Get pointers to all tables in the database
-		std::vector<Table*> GetTables();
+		DATABASE_API std::vector<Table*> GetTables();
 
 		// Query to return a table
-		Table* Query(std::string SELECT, std::string FROM, std::string WHERE);
+		DATABASE_API Table* Query(std::string SELECT, std::string FROM, std::string WHERE);
 
 	};
 }
