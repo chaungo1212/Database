9e08a70ab2f7e9801734d0e84b3ee18a6fb6a153
chaungo <chaungo@tamu.edu>
2017-02-28 22:16:00 -0600

    update Table class

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -5,68 +5,154 @@
 #include <iostream>
 #include "Database.h"  
 
-namespace Database
-{
+namespace Database {
 	Record::Record(int size)
 	{
+		this->next = NULL;
+		this->values.resize(size);
 	}
 
 	int Record::GetSize()
 	{
-		return 0;
+		return this->values.size();
 	}
 
 	std::string Record::Get(int index)
 	{
-		return std::string();
+		return this->values[index];
 	}
 
 	void Record::Set(int index, std::string new_value)
 	{
+		this->values[index] = new_value;
+	}
+
+	Record Record::Join(Record record2) {
+		Record join_record(this->GetSize() + record2.GetSize());
+		for (int i = 0; i < this->GetSize(); i++)
+			join_record.Set(i, this->Get(i));
+		for (int i = 0; i < record2.GetSize(); i++)
+			join_record.Set(i + this->GetSize(), record2.Get(i));
+		return join_record;
 	}
 
 	Table::Table()
 	{
+		this->head = NULL;
 	}
 
 	Table::Table(std::vector<std::string> attributes)
 	{
+		this->head = NULL;
+		this->attributes = attributes;
 	}
 
 	void Table::AddAttribute(std::string attribute_name)
 	{
+		bool find = false;
+		for (int i = 0; i < this->attributes.size(); i++) {
+			if (this->attributes[i].compare(attribute_name) == 0) {
+				//attribute_name is found
+				find = true;
+				break;
+			}
+		}
+		if (find == false)
+			this->attributes.push_back(attribute_name);
 	}
 
 	void Table::DeleteAttribute(std::string attribute_name)
 	{
+		for (int i = 0; i < this->attributes.size(); i++) {
+			if (this->attributes[i].compare(attribute_name) == 0) {
+				//we find attribute_name and delete it
+				this->attributes.erase(this->attributes.begin() + i);
+				return;
+			}
+		}
+		std::cout << "Attribute: " << attribute_name << " not found." << std::endl;
+		return;
 	}
 
 	void Table::InsertRecord(Record new_record)
 	{
+		Record* _new_record = new Record(new_record.GetSize());
+		for (int i = 0; i < new_record.GetSize(); i++)
+			_new_record->Set(i, new_record.Get(i));
+		if (this->head == NULL) {
+			this->head = _new_record;
+		}
+		else {
+			Record* current = this->head;
+			while (current->next != NULL) {
+				current = current->next;
+			}
+			current->next = _new_record;
+		}
 	}
 
 	std::vector<std::string> Table::ListAttributes()
 	{
-		return std::vector<std::string>();
+		return this->attributes;
 	}
 
 	int Table::GetSize()
 	{
-		return 0;
+		int size = 0;
+		Record* current = this->head;
+		while (current != NULL) {
+			size++;
+			current = current->next;
+
+		}
+		return size;
 	}
 
 	Record * Table::GetFirstRecord()
 	{
-		return nullptr;
+		return this->head;
 	}
 
 	void Table::DesignateKey(std::string key_name)
 	{
+		bool find = false;
+		for (int i = 0; i < this->attributes.size(); i++) {
+			if (this->attributes[i].compare(key_name) == 0) {
+				this->key_attribute = key_name;
+				find = true;
+				break;
+			}
+		}
+		if (find == false) {
+			std::cout << "Key_name " << key_name << " is not found" << std::endl;
+		}
 	}
 
 	Table Table::CrossJoin(Table joining_table)
 	{
-		return Table();
+		Table join_table;
+		int t1_size = this->GetSize(); // # of records
+		int t2_size = joining_table.GetSize();
+		int t1_nattrs = this->attributes.size(); // # of attributes
+		int t2_nattrs = joining_table.attributes.size();
+		Record* current1_ptr = this->head;
+		Record* current2_ptr;
+		Record current1(t1_nattrs);
+		Record current2(t2_nattrs);
+		while (current1_ptr != NULL) {
+			current2_ptr = joining_table.head;
+			for (int i = 0; i < t1_nattrs; i++)
+				current1.Set(i, current1_ptr->values[i]);
+			while (current2_ptr != NULL) {
+				for (int i = 0; i < t2_nattrs; i++)
+					current2.Set(i, current2_ptr->values[i]);
+				Record join_record = current1.Join(current2);
+				join_table.InsertRecord(join_record);
+				current2_ptr = current2_ptr->next;
+			}
+			current1_ptr = current1_ptr->next;
+		}
+		return join_table;
 	}
 
 	Table Table::NaturalJoin(Table joining_table)
@@ -89,12 +175,23 @@ namespace Database
 		return 0;
 	}
 
-	/*
-		Begin database functions
-	*/
+	void Table::Print() {
+		std::cout << "----------------------------------------------------\n";
+		Record* current = this->head;
+		std::vector<std::string> values;
+		while (current != NULL) {
+			values = current->values;
+			for (int i = 0; i < values.size(); i++)
+				std::cout << values[i] << " ";
+			std::cout << std::endl;
+			current = current->next;
+		}
+		std::cout << "----------------------------------------------------\n";
+	}
+
 	Database::Database()
 	{
-		
+
 	}
 
 	void Database::Add(std::string table_name, Table* table)
@@ -111,7 +208,7 @@ namespace Database
 	{
 		std::vector<std::string> table_names;
 		std::map<std::string, Table*>::iterator it = all_tables.begin();
-			
+
 		while (it != all_tables.end())
 		{
 			table_names.push_back(it->first);
